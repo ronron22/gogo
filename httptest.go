86e738e8ccfcc36df8997d8b6d1d70f7bcb6c2ba
antonio <antonio@lapeton.nv>
2018-03-19 19:40:50 +0100

    simple http server

---
+++ httptest.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net"	
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("X-tagada: ", "pouet")
+	w.WriteHeader(http.StatusOK)
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Length", fmt.Sprint(len(ip)))
+	fmt.Fprint(w, string(ip))
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
