9af77f5f9212a0f606ddf745ea62ff5c12e7e4b0
antonio <antonio@lapeton.nv>
2018-03-20 13:37:11 +0100

    add showmyip.go

---
+++ showmyip.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/Tomasen/realip"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("X-tagada: ", "pouet")
+	w.WriteHeader(http.StatusOK)
+	// recup du xforwardedfor 
+	clientIP := realip.FromRequest(r)
+	w.Header().Set("Content-Length", fmt.Sprint(len(clientIP)))
+	fmt.Fprint(w, clientIP)
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
